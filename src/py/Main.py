d3e39033690e60eb072b84d2f4686aec068ff06a
michaelvollmin <michael.vollmin@etu.unige.ch>
2021-11-16 23:24:58 +0100

    created games df

--- src/py/Main.py
+++ src/py/Main.py
@@ -9,11 +9,20 @@ from Parser import parser
 
 class Main(object):
     def __init__(self):
-        df = pd.DataFrame()
-        filepaths = (natsorted(glob.glob('../../in/xlsx/*.xlsx')))
+        self.raw_data = pd.DataFrame()
+        filepaths = (natsorted(glob.glob('../../in/xlsx/*2019*.xlsx')))
         for filepath in filepaths:
-            df = df.append(parser.read_xlsx(filepath),ignore_index=True)
-        df.to_html('../../out/html/combined.html')
+            self.raw_data = self.raw_data.append(parser.read_xlsx(filepath),ignore_index=True)
+        self.games = pd.DataFrame()
+        for i in range(0,len(self.raw_data),2):
+            row = pd.DataFrame()
+            row['Visitor'] = [self.raw_data.at[i,'Team']]
+            row['Home'] = [self.raw_data.at[i+1, 'Team']]
+            row['Score'] = [self.raw_data.at[i, 'Final']- self.raw_data.at[i+1, 'Final']]
+            self.games = self.games.append(row, ignore_index=True)
+
+        self.raw_data.to_html('../../out/html/combined.html')
+        self.games.to_html('../../out/html/games.html')
 
 if __name__ == '__main__':
     main = Main()
