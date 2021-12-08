e2ea63748521d666c1d9968c70d8a0e0bcffa51a
Marcos Oliveira <marcosoliveira@cern.ch>
2021-12-08 16:21:14 -0300

    fixing the loop

--- src/py/Main.py
+++ src/py/Main.py
@@ -43,14 +43,21 @@ class Main(object):
                 self.raw_data.at[i, 'Contract Momentum'] = self.raw_data.at[i, 'Momentum'] - self.raw_data.at[i+1, 'Momentum']
                 self.raw_data.at[i+1, 'Contract Momentum'] = self.raw_data.at[i+1, 'Momentum'] - self.raw_data.at[i, 'Momentum']
 ###Attempt to make Lng strategy column (Using bets of $100)
-                for i in range(self.raw_data['Contract Momentum']):
-                    if i >= 15:
-                        if self.raw_data['Score'] > 0:
-                    self.raw_data['Long Strategy'] = 0
-                            if self.raw_data['Open'] < 0:
-                        self.raw_data['Long Strategy'] = -100
-                                self.raw_data['Long Strategy'] = (1 - (100 / self.raw_data['Open']))*100
-                            self.raw_data['Long Strategy'] = ((self.raw_data['Open'] / 100) + 1)*100
+        for i in range(len(self.raw_data)):
+            if self.raw_data.at[i,'Contract Momentum'] >= 15:
+                if self.raw_data['Score'] > 0:
+                    if self.raw_data['Open'] < 0:
+                        self.raw_data['Long Strategy'] = (1 - (100 / self.raw_data['Open'])) * 100
+                    else:
+                        self.raw_data['Long Strategy'] = ((self.raw_data['Open'] / 100) + 1) * 100
+                else:
+                    self.raw_data['Long Strategy'] = -100
+            else:
+                self.raw_data['Long Strategy'] = 0
+
+
+
+
 
 
 #Plotting Contract Momentum
