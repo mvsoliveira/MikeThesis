d19dc3157b5e859316ad15d4ec693db5844aa73f
mikevollmin <michael.vollmin@etu.unige.ch>
2022-04-24 12:12:37 -0500

    efficiency portfolio is established, just have to get the trading strategy in there

--- src/py/Efficient.py
+++ src/py/Efficient.py
@@ -4,6 +4,7 @@ import datetime as dt
 from pandas_datareader import data as pdr
 import matplotlib.pyplot as plt
 import scipy.optimize as sc
+import plotly.graph_objs as go
 
 
 import pandas as pd
@@ -14,7 +15,7 @@ df = pd.read_csv("Percentchange.csv", usecols = ['SP','BofA','MCSI'])
 #stocks= (stock+ '.AX' for stock in stocklist)
 meanreturns= df.mean()
 meanreturnslist= [asset for asset in meanreturns]
-numAssets= len(meanreturns)
+numAssets= len(meanreturnslist)
 weights= np.array([.03,.03,.04])
 
 #print(meanreturnslist)
@@ -33,7 +34,7 @@ def portfolioperformance(weights, meanreturns, covmatrix):
 
 
 
-weights= np.array([.03,.03,.04])
+#weights= np.array([.03,.03,.04])
 returns, std = portfolioperformance(weights, meanreturns, covmatrix)
 print(round(returns*100,2), round(std*100,2))
 
@@ -55,8 +56,8 @@ def maxSR(meanreturns, covmatrix, riskFreeRate = 0, constraintSet=(0,1)):
 
 
 result = maxSR(meanreturns, covmatrix)
-maxSR, maxWeights= result['fun'], result['x']
-print(maxSR, maxWeights)
+maxSRvalue, maxWeights= result['fun'], result['x']
+print(maxSRvalue, maxWeights)
 
 
 def portfolioVariance(weights, meanreturns, covmatrix):
@@ -73,6 +74,7 @@ def minimizeVariance(meanreturns, covmatrix, constraintSet=(0,1)):
     bounds = tuple(bound for asset in range(numAssets))
     result = sc.minimize(portfolioVariance, numAssets*[1./numAssets], args=args,
                         method='SLSQP', bounds=bounds, constraints=constraints)
+
     return result
 
 
@@ -87,7 +89,7 @@ def portfolioReturn(weights, meanreturns, covmatrix):
 
 def efficientOpt(meanreturns, covmatrix, returnTarget, constraintSet=(0,1)):
     """For each returnTarget, we want to optimise the portfolio for min variance"""
-    numAssets = meanreturns
+    #numAssets = meanreturns
     args = (meanreturns, covmatrix)
 
     constraints = ({'type':'eq', 'fun': lambda x: portfolioReturn(x, meanreturns, covmatrix) - returnTarget},
@@ -104,14 +106,16 @@ def calculatedResults(meanreturns, covmatrix, riskFreeRate=0, constraintSet=(0, 
     # Max Sharpe Ratio Portfolio
     maxSR_Portfolio = maxSR(meanreturns, covmatrix)
     maxSR_returns, maxSR_std = portfolioperformance(maxSR_Portfolio['x'], meanreturns, covmatrix)
-    maxSR_returns, maxSR_std = round(maxSR_returns * 100, 2), round(maxSR_std * 100, 2)
+    maxSR_returns, maxSR_std = maxSR_returns, maxSR_std
+    #maxSR_returns, maxSR_std = round(maxSR_returns * 100, 2), round(maxSR_std * 100, 2)
     maxSR_allocation = pd.DataFrame(maxSR_Portfolio['x'], index=meanreturns.index, columns=['allocation'])
     maxSR_allocation.allocation = [round(i * 100, 0) for i in maxSR_allocation.allocation]
 
     # Min Volatility Portfolio
     minVol_Portfolio = minimizeVariance(meanreturns, covmatrix)
     minVol_returns, minVol_std = portfolioperformance(minVol_Portfolio['x'], meanreturns, covmatrix)
-    minVol_returns, minVol_std = round(minVol_returns * 100, 2), round(minVol_std * 100, 2)
+    minVol_returns, minVol_std = minVol_returns, minVol_std
+    #minVol_returns, minVol_std = round(minVol_returns * 100, 2), round(minVol_std * 100, 2)
     minVol_allocation = pd.DataFrame(minVol_Portfolio['x'], index=meanreturns.index, columns=['allocation'])
     minVol_allocation.allocation = [round(i * 100, 0) for i in minVol_allocation.allocation]
 
@@ -121,7 +125,65 @@ def calculatedResults(meanreturns, covmatrix, riskFreeRate=0, constraintSet=(0, 
     for target in targetReturns:
         efficientList.append(efficientOpt(meanreturns, covmatrix, target)['fun'])
 
-    return maxSR_returns, maxSR_std, maxSR_allocation, minVol_returns, minVol_std, minVol_allocation, efficientList
+    return maxSR_returns, maxSR_std, maxSR_allocation, minVol_returns, minVol_std, minVol_allocation, efficientList, targetReturns
 
 
 print(calculatedResults(meanreturns, covmatrix))
+
+
+def EF_graph(meanreturns, covmatrix, riskFreeRate=0, constraintSet=(0, 1)):
+    """Return a graph ploting the min vol, max sr and efficient frontier"""
+    maxSR_returns, maxSR_std, maxSR_allocation, minVol_returns, minVol_std, minVol_allocation, efficientList, targetReturns = calculatedResults(meanreturns, covmatrix, riskFreeRate, constraintSet)
+
+    # Max SR
+    MaxSharpeRatio = go.Scatter(
+        name='Maximium Sharpe Ratio',
+        mode='markers',
+        x=[maxSR_std],
+        y=[maxSR_returns],
+        marker=dict(color='red', size=14, line=dict(width=3, color='black'))
+    )
+
+    # Min Vol
+    MinVol = go.Scatter(
+        name='Mininium Volatility',
+        mode='markers',
+        x=[minVol_std],
+        y=[minVol_returns],
+        marker=dict(color='green', size=14, line=dict(width=3, color='black'))
+    )
+
+    # Efficient Frontier
+    EF_curve = go.Scatter(
+        name='Efficient Frontier',
+        mode='lines',
+        x=[ef_std for ef_std in efficientList],
+        y=[target for target in targetReturns],
+        #x=[round(ef_std * 100, 2) for ef_std in efficientList],
+        #y=[round(target * 100, 2) for target in targetReturns],
+        line=dict(color='black', width=4, dash='dashdot')
+    )
+
+    data = [MaxSharpeRatio, MinVol, EF_curve]
+
+    layout = go.Layout(
+        title='Portfolio Optimisation with the Efficient Frontier (without NHL gambling strategy)',
+        yaxis=dict(title='Annualised Return (%)'),
+        xaxis=dict(title='Annualised Volatility (%)'),
+        showlegend=True,
+        legend=dict(
+            x=0.75, y=0, traceorder='normal',
+            bgcolor='#E2E2E2',
+            bordercolor='black',
+            borderwidth=2),
+        width=800,
+        height=600)
+
+    fig = go.Figure(data=data, layout=layout)
+    return fig.show()
+
+EF_graph(meanreturns, covmatrix)
+
+
+
+
